007661897d4f1f696bb397ad8e3ab19ee6fd5a36
Mystikfluu <Mystikfluu@gmail.com>
2022-01-31 17:26:26 +0100

    added more words to word_array, function lookup should be faster now

--- codezt.lua
+++ codezt.lua
@@ -41,10 +41,20 @@ local input_file = io.open(input_filename,"r")
 
 io.input(input_file)
 
+local types = {
+  ["nil"          ] = true,
+  ["number"       ] = true,
+  ["bool"         ] = true,
+  ["function_ptr" ] = true,
+  ["string"       ] = true,
+  ["table"        ] = true
+}
+
 local stack  = {}
 local last = 0
 function push(t)
-  assert(type(t)=="table" and #t==2,"compiler error when pushing")
+  assert(type(t)=="table" and #t==2,"interpreter error when pushing")
+  assert(types[t[1]]==true,"unknown type got pushed")
   last = last + 1
   stack[last]=t
 end
@@ -58,16 +68,16 @@ function unsafe_pop()
 end
 
 function show_stack()
-  local str = {}
+  local str = {""}
   str[#str+1] = concat({"number of elements on the stack:",last},"\t")
 
   if(last ~= 0) then
     str[#str+1] = "elements on the stack:"
     for i=1,last do
-      str[#str+1] = concat({"type:",stack[i][1],"value:",stack[i][2]},"\t")
+      str[#str+1] = concat({"type:",stack[i][1],"value:",tostring(stack[i][2])},"\t")
     end
   end
-  print(concat(str,"\n[INFO]"))
+  print(concat(str,"\n[INFO]\t"))
 end
 
 local values = {}
@@ -200,6 +210,179 @@ word_array["end"] = function()
   error("unknown `end` found")
 end
 
+word_array["httpget"] = function()
+  local body, code, headers, status = http.request(pop()[2])
+  push({"string",status})
+  push({"table",headers})
+  push({"number",code})
+  push({"string",body})
+end
+
+word_array["drop"] = function()
+  pop()
+end
+
+word_array["unsafe_drop"] = function()
+  unsafe_pop()
+end
+
+word_array["print"] = function()
+  p1 = pop()
+  print(p1[2])
+end
+
+word_array["debug"] = function()
+  p1 = pop()
+  print(p1[2])
+  push(p1)
+end
+
+word_array["dup"] = function()
+  p1 = pop()
+  push(p1)
+  push(p1)
+end
+
+word_array["unsafe_dup"] = function()
+  p1 = unsafe_pop()
+  push(p1)
+  push(p1)
+end
+
+word_array["over"] = function()
+  p1 = pop()
+  p2 = pop()
+  push(p2)
+  push(p1)
+  push(p2)
+end
+
+word_array["2dup"] = function()
+  p1 = pop()
+  p2 = pop()
+  push(p2)
+  push(p1)
+  push(p2)
+  push(p1)
+end
+
+word_array["=="] = function()
+  p1 = pop()
+  p2 = pop()
+  push({"bool",p1[2]==p2[2]})
+end
+
+word_array["!="] = function()
+  p1 = pop()
+  p2 = pop()
+  push({"bool",p1[2]~=p2[2]})
+end
+word_array["~="] = word_array["!="]
+
+word_array["==="] = function()
+  p1 = pop()
+  p2 = pop()
+  push({"bool",p1[1]==p2[1] and p1[2]==p2[2]})
+end
+
+word_array["!=="] = function()
+  p1 = pop()
+  p2 = pop()
+  push({"bool",not (p1[1]==p2[1] and p1[2]==p2[2])})
+end
+word_array["~=="] = word_array["!=="]
+
+word_array["not"] = function()
+  p1 = pop()
+  assert(p1[1] == "bool","'not' is only usable with type 'bool'")
+  push({"bool",not p1[2]})
+end
+word_array["!"] = word_array["not"]
+
+word_array["null"] = function()
+  push({"nil","nil"})
+end
+word_array["nil"] = word_array["null"]
+word_array["undefined"] = word_array["null"]
+word_array["nix"] = word_array["null"]
+word_array["leer"] = word_array["null"]
+word_array["zerotwonotfound"] = word_array["null"]
+word_array["no"] = word_array["null"]
+word_array["luft"] = word_array["null"]
+
+word_array["and"] = function()
+  p1 = pop()
+  p2 = pop()
+  assert(p1[1] == "bool" and p2[1] == p1[1],"'not' is only usable with type 'bool'")
+  push({"bool",p1[2]and p2[2]})
+end
+word_array["&&"] = word_array["and"]
+
+word_array["or"] = function()
+  p1 = pop()
+  p2 = pop()
+  assert(p1[1] == "bool" and p2[1] == p1[1],"'not' is only usable with type 'bool'")
+  push({"bool",p1[2]or p2[2]})
+end
+
+word_array["!!"] = show_stack
+--smirk
+
+word_array["switch"] = function()
+  p1 = pop()
+  p2 = pop()
+  push(p1)
+  push(p2)
+end
+
+word_array["="] = function()
+  set_value = true
+end
+
+word_array["set"] = function()
+  p1 = pop()
+  if(values[last_word]) then
+    pop()
+  end
+  values[last_word]=p1[2]
+end
+
+word_array["clear"] = function()
+  stack={}
+  last=0
+end
+
+word_array["func"] == function()
+  collect = {"func"}
+end
+
+word_array["repeat"] == function()
+  collect = {"repeat"}
+
+word_array["if"] = function()
+  p1 = pop()
+  if(not (p1[2]==true and p1[1]=="bool")) then --can you simplify this
+    collect = {"ignore-if"}
+  end
+end
+
+word_array["include"] = function()
+  p1 = pop()
+  assert(p1[1]=="string","when including files, top of the stack must be a string")
+  local included_file = io.open(p1[2],"r")
+  run_file(included_file)
+end
+word_array["require"] = word_array["include"]
+word_array["import"] = word_array["include"]
+
+word_array["read"] = function()
+  push({"string",io.stdin:read()})
+end
+
+word_array["write"] = function()
+  io.stdout:write(pop()[2])
+end
+
 --[[
 
 
@@ -267,108 +450,6 @@ function run_line(line)
           collect_string = {sub(word,2,#word+1)}
         elseif(sub(word,1,1)=='"' and sub(word,#word,#word) == '"') then
             push({"string",sub(word,2,#word-1)})
-        elseif(word=="httpget") then
-          local body, code, headers, status = http.request(pop()[2])
-          push({"string",status})
-          push({"table",headers})
-          push({"number",code})
-          push({"string",body})
-        elseif(word=="dup") then
-          p1 = pop()
-          push(p1)
-          push(p1)
-        elseif(word=="unsafe_dup") then
-          p1 = unsafe_pop()
-          push(p1)
-          push(p1)
-        elseif(word=="over") then
-          p1 = pop()
-          p2 = pop()
-          push(p2)
-          push(p1)
-          push(p2)
-        elseif(word=="2dup") then
-          p1 = pop()
-          p2 = pop()
-          push(p2)
-          push(p1)
-          push(p2)
-          push(p1)
-        elseif(word=="drop") then
-          pop()
-        elseif(word=="unsafe_drop") then
-          unsafe_pop()
-        elseif(word=="print") then
-          p1 = pop()
-          print(p1[2])
-        elseif(word=="debug") then
-          p1 = pop()
-          print(p1[2])
-          push(p1)
-        elseif(word=="==") then
-          p1 = pop()
-          p2 = pop()
-          push({"bool",p1[2]==p2[2]})
-        elseif(word=="!=" or word=="~=") then
-          p1 = pop()
-          p2 = pop()
-          push({"bool",p1[2]~=p2[2]})
-        elseif(word=="===") then
-          p1 = pop()
-          p2 = pop()
-          push({"bool",p1[1]==p2[1] and p1[2]==p2[2]})
-        elseif(word=="!==" or word=="~==") then
-          p1 = pop()
-          p2 = pop()
-          push({"bool",not (p1[1]==p2[1] and p1[2]==p2[2])})
-        elseif(word=="not" or word=="!") then
-          p1 = pop()
-          assert(p1[1] == "bool","'not' is only usable with type 'bool'")
-          push({"bool",not p1[2]})
-        elseif(word=="and" or word=="&&") then
-          p1 = pop()
-          p2 = pop()
-          assert(p1[1] == "bool" and p2[1] == p1[1],"'not' is only usable with type 'bool'")
-          push({"bool",p1[2]and p2[2]})
-        elseif(word=="or") then
-          p1 = pop()
-          p2 = pop()
-          assert(p1[1] == "bool" and p2[1] == p1[1],"'not' is only usable with type 'bool'")
-          push({"bool",p1[2]or p2[2]})
-        elseif(word=="!!") then
-          show_stack()
-        elseif(word=="switch") then
-          p1 = pop()
-          p2 = pop()
-          push(p1)
-          push(p2)
-        elseif(word=="=") then
-          set_value = true
-        elseif(word=="set") then
-          p1 = pop()
-          if(values[last_word]) then
-            pop()
-          end
-          values[last_word]=p1[2]
-        elseif(word=="clear") then
-          stack={}
-          last=0
-        elseif(word=="func" or word=="repeat") then
-          collect = {word}
-        elseif(word=="if") then
-          p1 = pop()
-          if(not (p1[2]==true and p1[1]=="bool")) then
-            collect = {"ignore-if"}
-          end
-        elseif(word=="include" or word=="require" or word=="import") then
-          p1 = pop()
-          assert(p1[1]=="string","when including files, top of the stack must be a string")
-          local included_file = io.open(p1[2],"r")
-          run_file(included_file)
-        elseif(word=="read") then
-          push({"string",io.stdin:read()})
-        elseif(word=="write") then
-          io.stdout:write(pop()[2])
         elseif(sub(word,1,2)=="//" or word=="return") then
           break
         elseif(functions[sub(word,1,#word-2)]) then
@@ -398,15 +479,15 @@ function run_file(file)
     collection = {}
     run_line(line)
   end
-  if(last ~= 0) then
-    warn("number of items on the stack must be 0 after full execution")
-    warn("stack:")
-    show_stack()
-  end
 end
 
 local start = os.clock()
 run_file(input_file)
+if(last ~= 0) then
+  warn("number of items on the stack must be 0 after full execution")
+  warn("stack:")
+  show_stack()
+end --after all files ran (even imports)
 info(os.clock()-start,"s to run the program")
 
 io.close(input_file)
